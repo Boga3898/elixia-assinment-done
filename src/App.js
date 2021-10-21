f2e44d4b5b5f49e3036f831838be0d75e14c3e22
Boga3898 <shravaniboga@gmail.com>
2021-10-22 00:01:19 +0530

    My Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+export default class App extends Component {
+	state = {
+		flagImg: '//restcountries.com/data/png/per.png',
+	};
+	selectCountryHandler = (e) => {
+		this.setState({ countryCodeValue: e.target.value });
+	};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	countryFlagHandler = () => {
+		const select = document.querySelector('#country');
+		this.setState({
+			flagImg: `https://flagpedia.net/data/flags/h80/${select.selectedOptions[0].dataset.countrycode.toLowerCase()}.webp`
+		});
+	};
+
+	render() {
+		return (
+			<div className="page">
+        <h1>Choose the Country</h1>
+						<div className="select-box" onChange={this.countryFlagHandler}>
+              <div className="div1">
+              <p>Country Flag</p>
+							<img src={this.state.flagImg} alt="country-flag" id="img" className="flag-img" />
+              </div>
+              <div className="div2"> 
+              <p>Calling code:</p>
+              <p className="same">{this.state.countryCodeValue}</p>
+              </div>
+							<select
+								id="country"
+								onChange={this.selectCountryHandler}
+								defaultValue={this.state.countryCodeValue}
+							>
+								<option value="" hidden>
+									Select Country
+								</option>
+								<option data-countryCode="IN" value="91">
+									India 
+								</option>
+								<option data-countryCode="CH" value="86">
+									China
+								</option>
+								<option data-countryCode="SK" value="82">
+									South Koria 
+								</option>
+								<option data-countryCode="JP" value="81">
+									  Japan
+								</option>
+                </select>
+						</div>
+					
+				
+			
+				</div>
+		
+		);
+	}
 }
-
-export default App;
